8bea7464c78b0065cb4b4765f54f62f9e20becdf
π <razzester86@gmail.com>
2021-06-25 14:14:17 -0600

    Added new data.

--- data/data_generator.py
+++ data/data_generator.py
@@ -68,25 +68,180 @@ class DiffVisualizer:
     
     @staticmethod
     def matching_lines(_final, _snap):
-        d = difflib.Differ()
-        diff = d.compare(_final.splitlines(True), _snap.splitlines(True))
-        # print('\n'.join(diff))
-        blocks = []
-        # i is the first file's index, j the second's
-        i = 0;
-        j = 0;
-        for line in diff:
-            start = line[0]
-            line = line[2:]
-            if (start == ' '):
-                blocks.append((i, j, len(line)))
-                i += len(line)
-                j += len(line)
-            elif (start == '-'):
-                i += len(line)
-            elif (start == '+'):
-                j += len(line)
+        class DiffHelper:
+            def __init__(self, charjunk=None):
+                self.charjunk = charjunk
+
+            def _fancy_replace(self, abidx, a, alo, ahi, b, blo, bhi):
+                r"""
+                When replacing one block of lines with another, search the blocks
+                for *similar* lines; the best-matching pair (if any) is used as a
+                synch point, and intraline difference marking is done on the
+                similar pair. Lots of work, but often worth it.
+                Example:
+                >>> d = Differ()
+                >>> results = d._fancy_replace(['abcDefghiJkl\n'], 0, 1,
+                ...                            ['abcdefGhijkl\n'], 0, 1)
+                >>> print(''.join(results), end="")
+                - abcDefghiJkl
+                ?    ^  ^  ^
+                + abcdefGhijkl
+                ?    ^  ^  ^
+                """
+
+                # don't synch up unless the lines have a similarity score of at
+                # least cutoff; best_ratio tracks the best score seen so far
+                best_ratio, cutoff = 0.74, 0.75
+                cruncher = difflib.SequenceMatcher(self.charjunk)
+                eqi, eqj = None, None  # 1st indices of equal lines (if any)
+
+                # search for the pair that matches best without being identical
+                # (identical lines must be junk lines, & we don't want to synch up
+                # on junk -- unless we have to)
+                for j in range(blo, bhi):
+                    bj = b[j]
+                    cruncher.set_seq2(bj)
+                    for i in range(alo, ahi):
+                        ai = a[i]
+                        if ai == bj:
+                            if eqi is None:
+                                eqi, eqj = i, j
+                            continue
+                        cruncher.set_seq1(ai)
+                        # computing similarity is expensive, so use the quick
+                        # upper bounds first -- have seen this speed up messy
+                        # compares by a factor of 3.
+                        # note that ratio() is only expensive to compute the first
+                        # time it's called on a sequence pair; the expensive part
+                        # of the computation is cached by cruncher
+                        if cruncher.real_quick_ratio() > best_ratio and \
+                                cruncher.quick_ratio() > best_ratio and \
+                                cruncher.ratio() > best_ratio:
+                            best_ratio, best_i, best_j = cruncher.ratio(), i, j
+                if best_ratio < cutoff:
+                    # no non-identical "pretty close" pair
+                    if eqi is None:
+                        # no identical pair either -- treat it as a straight replace
+                        # TODO fix this
+                        #                 print('here')
+                        #                 yield None
+                        abidx[0] += ahi - alo
+                        abidx[1] += bhi - blo
+                        #                 yield from self._plain_replace(a, alo, ahi, b, blo, bhi)
+                        return
+                    # no close pair, but an identical pair -- synch up on that
+                    best_i, best_j, best_ratio = eqi, eqj, 1.0
+                else:
+                    # there's a close pair, so forget the identical pair (if any)
+                    eqi = None
+
+                # a[best_i] very similar to b[best_j]; eqi is None iff they're not
+                # identical
+
+                # pump out diffs from before the synch point
+                yield from self._fancy_helper(abidx, a, alo, best_i, b, blo, best_j)
+
+                # do intraline marking on the synch pair
+                aelt, belt = a[best_i], b[best_j]
+                #         print('synch pair:', aelt, belt)
+                if eqi is None:
+                    # pump out a '-', '?', '+', '?' quad for the synched lines
+                    atags = btags = ""
+                    cruncher.set_seqs(aelt, belt)
+                    for tag, ai1, ai2, bj1, bj2 in cruncher.get_opcodes():
+                        la, lb = ai2 - ai1, bj2 - bj1
+                        #                 print('*', tag, abidx)
+                        if tag == 'replace':
+                            atags += '^' * la
+                            btags += '^' * lb
+                            abidx[0] += la
+                            abidx[1] += la
+                        elif tag == 'delete':
+                            atags += '-' * la
+                            abidx[0] += la
+                        elif tag == 'insert':
+                            btags += '+' * lb
+                            abidx[1] += lb
+                        elif tag == 'equal':
+                            yield (abidx[0], abidx[1], la)
+                            abidx[0] += la
+                            abidx[1] += la
+                            atags += ' ' * la
+                            btags += ' ' * lb
+                        else:
+                            raise ValueError('unknown tag %r' % (tag,))
+                #             yield from self._qformat(aelt, belt, atags, btags)
+                else:
+                    # the synch pair is identical
+                    # TODO fix
+                    yield (abidx[0], abidx[1], len(aelt))
+                #             yield '  ' + aelt
+
+                # pump out diffs from after the synch point
+
+                yield from self._fancy_helper(abidx, a, best_i + 1, ahi, b, best_j + 1, bhi)
+
+            def _fancy_helper(self, abidx, a, alo, ahi, b, blo, bhi):
+                g = []
+                if alo < ahi:
+                    if blo < bhi:
+                        #                 g = self._fancy_replace(abidx, a, alo, ahi, b, blo, bhi)
+                        yield from self._fancy_replace(abidx, a, alo, ahi, b, blo, bhi)
+                    else:
+                        abidx[0] += ahi - alo
+                #                 print('dump')
+                #                 g = self._dump('-', a, alo, ahi)
+                elif blo < bhi:
+                    abidx[1] += bhi - blo
+        #             print('dump')
+        #             g = self._dump('+', b, blo, bhi)
+
+        #         yield from g
+
+        def test(linejunk, a, b):
+            blocks = []
+            cruncher = difflib.SequenceMatcher(linejunk, a, b)
+            ai = 0
+            bi = 0
+            helper = DiffHelper()
+            for tag, alo, ahi, blo, bhi in cruncher.get_opcodes():
+                astr = ''.join(a[alo:ahi])
+                bstr = ''.join(b[blo:bhi])
+                #         print(tag, alo, ahi, blo, bhi, astr, bstr)
+                if tag == 'replace':
+                    yield from helper._fancy_replace([ai, bi], a, alo, ahi, b, blo, bhi)
+                    ai += len(astr)
+                    bi += len(bstr)
+                #             g = self._fancy_replace(a, alo, ahi, b, blo, bhi)
+                elif tag == 'delete':
+                    # in a
+                    ai += len(astr)
+                #             g = self._dump('-', a, alo, ahi)
+                elif tag == 'insert':
+                    # in b
+                    bi += len(bstr)
+                #             g = self._dump('+', b, blo, bhi)
+                elif tag == 'equal':
+                    #             blocks.append((ai, bi, len(astr)))
+                    block = (ai, bi, len(astr))
+                    ai += len(astr)
+                    bi += len(astr)
+                    yield block
+                else:
+                    raise ValueError('unknown tag %r' % (tag,))
+    
+            return blocks
+    
+            #        0123 45678 90123 45678
+            #        012  3456  7890  1234  567890
+    
+        test1 = 'cat\ndogs\nfish\ngoat\ngerbil\n'
+        test2 = 'cat\ndots\nfis\ngoat\ngerbil\n'
+        newline = True
+        blocks = test(None, _final.splitlines(newline), _snap.splitlines(newline))
         return blocks
+        # for b in blocks:
+        #     print(b)
 
     @staticmethod
     def generate_grid_data(final_code, diff_list):
@@ -107,10 +262,11 @@ class DiffVisualizer:
             # current_code = '\n'.join(each)
             then = datetime.datetime.now()
             current_code = each
-            d=difflib.SequenceMatcher(None, current_code, final_code)
+            # d=difflib.SequenceMatcher(None, current_code, final_code)
             # d = difflib.SequenceMatcher(None, final_code, current_code)
-            mat = d.get_matching_blocks()
+            # mat = d.get_matching_blocks()
             # mat = DiffVisualizer.matching_lines(final_code, current_code)
+            mat = DiffVisualizer.matching_lines(final_code, current_code)
             points = [0 for each in range(0, final_code_len)]
             # display(mat)
             match_block_diff = []
@@ -131,19 +287,32 @@ class DiffVisualizer:
             #     current_start, current_size = get_start_size(each_match[1], each_match[2], splitted_snapshot)
             #     match_block_diff.append([current_start, current_size])
             #     match_block_final.append([start_index, size])
-            
-            for each_match in mat[:-1]:
-                initial_b = each_match.b
+
+            for each_match in mat:
+                initial_b = each_match[0]
                 # initial_b = each_match.a
-                size = each_match.size
+                size = each_match[2]
                 # print('Initial b size: ', initial_b, size)
                 for i in range(initial_b, initial_b + size):
                     points[i] = 1
                     grid_points.append([i, row])
-                match_block_diff.append([each_match.a, each_match.size])
-                match_block_final.append([each_match.b, each_match.size])
+                match_block_diff.append([each_match[1], each_match[2]])
+                match_block_final.append([each_match[0], each_match[2]])
                 # match_block_diff.append([each_match.b, each_match.size])
                 # match_block_final.append([each_match.a, each_match.size])
+            
+            # for each_match in mat[:-1]:
+            #     initial_b = each_match.b
+            #     # initial_b = each_match.a
+            #     size = each_match.size
+            #     # print('Initial b size: ', initial_b, size)
+            #     for i in range(initial_b, initial_b + size):
+            #         points[i] = 1
+            #         grid_points.append([i, row])
+            #     match_block_diff.append([each_match.a, each_match.size])
+            #     match_block_final.append([each_match.b, each_match.size])
+            #     # match_block_diff.append([each_match.b, each_match.size])
+            #     # match_block_final.append([each_match.a, each_match.size])
             grid_data.append(points)
             diff_match_blocks.append({
                 'final': match_block_final,
