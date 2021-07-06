8a0f73ae3eb08fe06c75bbf89497d49e0fdd89a5
π <razzester86@gmail.com>
2021-07-06 16:33:22 -0600

    Added cache headers back.

--- main.py
+++ main.py
@@ -24,6 +24,17 @@ def get_file(folder, file):
     file_dir = os.path.join(path, f"data/{folder}/{file}")
     return send_file(file_dir)
 
+# @app.after_request
+# def add_header(r):
+#     """
+#     Add headers to both force latest IE rendering engine or Chrome Frame,
+#     and also to cache the rendered page for 10 minutes.
+#     """
+#     r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+#     r.headers["Pragma"] = "no-cache"
+#     r.headers["Expires"] = "0"
+#     r.headers['Cache-Control'] = 'public, max-age=0'
+#     return r
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
